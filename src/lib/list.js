d20ddb3017cedc36b12e29897e33da16174d573f
EsterJenny <ejb7@hi.is>
2019-11-18 20:08:29 +0000

    tengdi index.html við css og js
    allar js og scss skrár eiga að vera tengdar

--- src/lib/list.js
+++ src/lib/list.js
@@ -3,9 +3,35 @@ import { empty } from './helpers';
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.url = 'lectures.json';
+  }
+  setContent(...content) {
+    empty(this.container);
+
+    content.forEach((item) => {
+      const contentToShow = typeof item === 'string'
+        ? document.createTextNode(item) : item;
+
+      this.container.appendChild(contentToShow);
+    });
+  }
+
+  loadLectures() {
+    return fetch(this.url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Gat ekki sótt fyrirlestra');
+        }
+        return res.json();
+      });
   }
 
   load() {
+    this.loadLectures()
+      .catch((error) => {
+        console.error(error);
+        this.setError(error.message);
+      });
     empty(this.container);
   }
 }
