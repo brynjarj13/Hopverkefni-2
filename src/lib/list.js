76b3f435fb4baebe5edae8379ab8873122c314b6
brynjarj13 <brynjar1993@gmail.com>
2019-11-27 17:58:28 +0000

    List.js update
    added eventlisters for buttons and one time remover of the lecture list for the first time a button is clicked to filter

--- src/lib/list.js
+++ src/lib/list.js
@@ -28,3 +28,57 @@ export default class List {
     empty(this.container);
   }
 }
+let listcontainer = document.querySelector('list');
+const button = document.querySelector('button');
+function listRemover(){
+  /*function that removes all lectures once before showing the one selected*/
+    while (listcontainer.firstChild) {
+      listcontainer.removeChild(listcontainer.firstChild);
+    }
+  button.removeEventListener('click', listRemover);
+}
+button.addEventListener('click', listRemover);
+
+const html__button =
+  document.querySelector('html__button');
+html__button.addEventListener('click', toggleHtml);
+
+const css__button =
+  document.querySelector('css__button');
+css__button.addEventListener('click', toggleCss);
+
+const javascript__button =
+  document.querySelector('javascript__button');
+javascript__button.addEventListener('click', toggleJavascript);
+
+let i = 0;
+function toggleHtml() {
+  if (i % 2 === 0) {
+    adds
+  }
+  else {
+    removes
+  }
+}
+
+let y = 0;
+function toggleCss() {
+  if (y % 2 === 0) {
+    adds
+  }
+  else {
+    removes
+  }
+}
+
+let x = 0;
+function toggleJavascript() {
+  if (x % 2 === 0) {
+    adds
+  }
+  else {
+    removes
+  }
+}
+
+
