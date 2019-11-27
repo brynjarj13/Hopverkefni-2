b9f421219f17c4587ae0f5e2c150a1c254e04dad
EsterJenny <ejb7@hi.is>
2019-11-27 22:16:34 +0000

    lecture.js

--- src/lib/lecture.js
+++ src/lib/lecture.js
@@ -1,5 +1,44 @@
-export default class Lecture { 
+export default class Lecture {
   constructor (){
     this.url = 'lectures.json';
+  }
+
+  loadLectures() {
+    return fetch(this.url)
+    .then((res) => {
+      return res.json();
+    });
+  }
+
+  createLecture(content) {
+
+    content.forEach((i) => {
+      let item;
+      switch (i.type) {
+        case 'youtube':
+        item = youtube(i.data);
+        break;
+        case 'text':
+        item = text(i.data);
+        break;
+        case 'list':
+        item = list(i.data);
+        break;
+        case 'heading':
+        item = heading(i.data);
+        break;
+        case 'code':
+        item = code(i.data);
+        break;
+        case 'quote':
+        item = quote(i.data, i.attribute);
+        break;
+        case 'image':
+        item = image(i.data, i.caption);
+        break;
+        default:
+        item = el('div', i.type);
+      }
+    }
   }
 }
